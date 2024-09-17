acf7e318984c1a18cfd580a750fd817df9676b92
Peter <petternds@gmail.com>
2024-09-17 18:44:58 -0300

    first commit

---
+++ controladores/livro.js
@@ -0,0 +1,98 @@
+const fs = require("fs");
+
+const {
+    getTodosLivros,
+    getLivroPorId,
+    insereLivro,
+    modificaLivro,
+    deletaLivroPorId
+} = require("../servicos/livro.js");
+const {
+    patch
+} = require("../rotas/livro.js");
+
+function getLivros(req, res) {
+    try {
+        const livros = getTodosLivros()
+        res.send(livros);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message)
+    }
+};
+
+function getLivro(req, res) {
+    try {
+        const id = req.params.id
+
+        if (id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro);
+        } else {
+            res.status(422);
+            res.send("Id inválido");
+        };
+    } catch (error) {
+        res.status(500);
+        res.send(error.message)
+    };
+};
+
+function postLivro(req, res) {
+    try {
+        const livroNovo = req.body;
+        if (req.body.nome) {
+            insereLivro(livroNovo);
+            res.status(201);
+            res.send("Livro inserido com sucesso");
+        } else {
+            res.status(422);
+            res.send("O campo nome é obrigatório")
+        };
+    } catch (error) {
+        res.status(500);
+        res.send(error.mensagem);
+    };
+};
+
+function patchLivro(req, res) {
+    try {
+        const id = req.params.id;
+
+        if (id && Number(id)) {
+            const body = req.body;
+            modificaLivro(body, id);
+            res.send("Item modificado com sucesso");
+        } else {
+            res.status(422);
+            res.send("Id inválido");
+        };
+    } catch (error) {
+        res.status(500);
+        res.send(error.mensagem);
+    };
+};
+
+function deleteLivro(req, res) {
+    try {
+        if (id && Number(id)) {
+            const id = req.params.id;
+            deletaLivroPorId(id)
+            res.send("Livro deletado com sucesso");
+        } else {
+            res.status(422);
+            res.send("Id inválido");
+        };
+    } catch (error) {
+        res.status(500);
+        res.send(error.mensagem);
+    };
+};
+
+module.exports = {
+    getLivros,
+    getLivro,
+    postLivro,
+    patchLivro,
+    deleteLivro
+};
