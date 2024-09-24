2b5aa111116b94469b7643f54bdef764e4e83b23
Peter <petternds@gmail.com>
2024-09-24 06:31:44 -0300

    criando a lista de livros favoritos

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const rotaLivro = require("./rotas/livro.js")
+const cors = require("cors")
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: "*"
+}))
 
 app.use('/livros', rotaLivro)
 
